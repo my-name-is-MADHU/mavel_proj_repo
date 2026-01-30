14afca040ce54ba2608854483de43874bd6070fd
User <bmadhureddy1999@gmail.com>
2026-01-30 22:28:25 +0530

    added requirements

--- CreateMavenProject/maven_file.py
+++ CreateMavenProject/maven_file.py
@@ -1,5 +1,5 @@
 print("hello, this is newly created file")
 print("hello, this is newly created file")
 print("hello, this is newly created file")
-print("hello, this is newly created file")
+print("hello, last trigger")
 
