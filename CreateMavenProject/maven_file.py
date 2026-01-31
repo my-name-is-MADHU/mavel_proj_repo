b35261e437a3d88d686fc79d2ef4b2aa627d266a
User <bmadhureddy1999@gmail.com>
2026-01-31 22:53:32 +0530

    kubernates pipelines

--- CreateMavenProject/maven_file.py
+++ CreateMavenProject/maven_file.py
@@ -2,6 +2,4 @@ print("hello, Last Test")
 print("hello, this is for deployment")
 print("hello, for dockers test")
 print("hello, test kubernates")
-print("hello, test kubernates")
-print("hello, test kubernates")
 
