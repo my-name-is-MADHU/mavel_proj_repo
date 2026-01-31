6cc7e6eb26811ec5019120d39a91458df7edb142
User <bmadhureddy1999@gmail.com>
2026-01-31 22:45:56 +0530

    Docker config

--- CreateMavenProject/maven_file.py
+++ CreateMavenProject/maven_file.py
@@ -1,9 +1,5 @@
 print("hello, Last Test")
 print("hello, this is for deployment")
 print("hello, for dockers test")
-print("hello, for dockers test")
-
-print("hello, test kubernates")
-print("hello, test kubernates")
 print("hello, test kubernates")
 
